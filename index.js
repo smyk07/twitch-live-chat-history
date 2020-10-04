a923b177fbe48123b449fcf2f2e73d4daaac12eb
jpiazzal <joel.piazzalungalerat@gmail.com>
2020-10-04 16:39:17 +0200

    Add the possibility to listen multiples twitch channels properly

--- index.js
+++ index.js
@@ -1,48 +1,57 @@
 "use strict";
 const chalk = require('chalk');
-
-if (!process.argv[2]) {
-	console.error(chalk.redBright("You need to add a Twitch channel name as first argument."));
-	console.info(chalk.yellowBright("eg: npm start TWITCH_CHANNEL_NAME"));
-	console.info(chalk.yellowBright("\n->  Check README.md for more informations!"));
-	process.exit(0);
-} else {
-	console.info(chalk.greenBright("App start successfully, you will see twitch chats soon..."));
-}
-
-const twitchChannel = process.argv.slice(2); 
-
 const tmi = require('tmi.js');
-
 const Datastore = require('nedb'); 
 
-const database = new Datastore('chats.db'); 
-database.loadDatabase();
+// add some security in case of there is no twitch channels as argument
+if (!process.argv[2]) {
+	console.error(chalk.redBright("You need to add a Twitch channel name as first argument."));
+	console.info(chalk.yellowBright("eg: npm start TWITCH_CHANNEL_NAME_1 TWITCH_CHANNEL_NAME_2"));
+	console.info(chalk.yellowBright("\n->  Check README.md for more informations!"));
+	process.exit(0);
+}
 
-const dropsDb = new Datastore('drops.db'); 
+// get twitch channels from arguments
+const twitchChannels = process.argv.slice(2); 
+
+// load databases
+const database = new Datastore('chats.db'); 
+const dropsDb = new Datastore('drops.db');
+database.loadDatabase();
 dropsDb.loadDatabase(); 
 
+// instanciate client to listen twitch channel
 const client = new tmi.Client({
 	connection: {
 		secure: true,
 		reconnect: true
 	},
-	channels: twitchChannel
-	// channels: [ 'codinggarden' ]
+	channels: twitchChannels
 });
 
+// connect client
 client.connect();
 
-client.on('message', (channel, tags, message, self) => {
-	// "Alca: Hello, World!"
+console.info(chalk.greenBright("App start successfully, you will see twitch chats soon..."));
 
-	console.log(chalk.cyanBright(`${tags['display-name']}: ${message}`));
+// listen messages from twitch channels
+client.on('message', (channel, tags, message, self) => {
+	// "#twitchChannel | Alca: Hello, World!"
+
+	console.log(chalk.cyanBright(`${channel} | ${tags['display-name']}: ${message}`));
 
 	// database.insert({socket_id: socket.id, time: socket.handshake.time}); 
-	database.insert({username: tags['display-name'], message: message, channel: twitchChannel}); 
+	database.insert({
+		channel,
+		message,
+		username: tags['display-name']
+	}); 
 
 	if (message === '!drop me') {
 		console.log(`${tags['display-name']} just dropped!`); 
-		dropsDb.insert({username: tags['display-name'], channel: twitchChannel}); 
+		dropsDb.insert({
+			channel,
+			username: tags['display-name']
+		}); 
 	}
 });
