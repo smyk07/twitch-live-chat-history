5edbd6a602e91da3518a67d6da1941331af283f2
Nicolas Hacala <nicolas.hacala@gmail.com>
2020-10-05 19:17:30 +0200

    #fix/scrolling: fix scrolling to bottom and only when user at bottom of chat

--- web/app.js
+++ web/app.js
@@ -3,10 +3,8 @@ const socket = io("http://localhost:8080");
 const renderChats = channels => {
   channels.forEach(c => {
     $('body').append(`
-      <div class="chats">
+      <div class="chats" id="${c.replace('#', '')}">
         <h3>${c}</h3>
-        <div id="${c.replace('#', '')}">
-        </div>
       </div>
     `)
   })
@@ -16,17 +14,21 @@ const renderMessage = (message) => {
   const {channel, message: m, tags} = message;
   const {'display-name': displayName, color, mod} = tags;
 
-  $(channel).append(`
-        <p>
-            <strong style="color: ${color}">${mod && '[MOD]'}${displayName}</strong>: ${m}
-        </p>
+  const chatRoom = $(channel);
+  const isScrolledToBottom = chatRoom[0].scrollHeight - chatRoom[0].clientHeight <= chatRoom[0].scrollTop + 1;
+
+  chatRoom.append(`
+    <p>
+      <strong style="color: ${color}">${mod ? '[MOD]' : ''}${displayName}</strong>: ${m}
+    </p>
   `);
-  $(channel).animate({"scrollTop": $(channel)[0].scrollHeight}, "fast");
+
+  if(isScrolledToBottom)
+    chatRoom.animate({"scrollTop": chatRoom[0].scrollHeight}, 1);
 };
 
 socket.on('channels', channels => renderChats(channels));
 
 socket.on('chat', message => {
-  console.log(message);
   renderMessage(message);
 })
