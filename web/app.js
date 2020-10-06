6e7322c23297223709f10c34ec32620fb4757f76
ankit9905 <56759402+ankit9905@users.noreply.github.com>
2020-10-06 13:24:05 +0530

    Update app.js

--- web/app.js
+++ web/app.js
@@ -1,6 +1,6 @@
-const socket = io("http://localhost:8080");
+const socket = io("http://localhost:8080"); // connecting using Socket io
 
-const renderChats = channels => {
+const renderChats = channels => {                           // collecting chats data from the app
   channels.forEach(c => {
     $('body').append(`
       <div class="chats" id="${c.replace('#', '')}">
@@ -15,7 +15,7 @@ const renderMessage = (message) => {
   const {'display-name': displayName, color, mod} = tags;
 
   const chatRoom = $(channel);
-  const isScrolledToBottom = chatRoom[0].scrollHeight - chatRoom[0].clientHeight <= chatRoom[0].scrollTop + 1;
+  const isScrolledToBottom = chatRoom[0].scrollHeight - chatRoom[0].clientHeight <= chatRoom[0].scrollTop + 1;   // forchecking if user is at the bottom or not
 
   chatRoom.append(`
     <p>
@@ -24,11 +24,11 @@ const renderMessage = (message) => {
   `);
 
   if(isScrolledToBottom)
-    chatRoom.animate({"scrollTop": chatRoom[0].scrollHeight}, 1);
+    chatRoom.animate({"scrollTop": chatRoom[0].scrollHeight}, 1);         
 };
 
-socket.on('channels', channels => renderChats(channels));
+socket.on('channels', channels => renderChats(channels));              //calling function for rendering channel
 
 socket.on('chat', message => {
-  renderMessage(message);
+  renderMessage(message);                                             //calling function for rendering message
 })
